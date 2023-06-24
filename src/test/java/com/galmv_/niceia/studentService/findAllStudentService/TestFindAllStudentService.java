62fe3e3504bf2458b666dbdc86eb813dd6964f1f
Gustavo-de-Almeida <gustavodealmeida01@gmail.com>
2023-06-23 23:10:02 -0300

    jenkins dockerFile and docker-compose cunfiguration created

---
+++ src/test/java/com/galmv_/niceia/studentService/findAllStudentService/TestFindAllStudentService.java
@@ -0,0 +1,33 @@
+package com.galmv_.niceia.studentService.findAllStudentService;
+
+import com.galmv_.niceia.student.StudentRepository;
+import com.galmv_.niceia.student.StudentService;
+import com.galmv_.niceia.student.entities.Student;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Set;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+public class TestFindAllStudentService {
+
+//    @Mock
+//    private StudentRepository repository;
+//    @InjectMocks
+//    private final StudentService service = new StudentService(this.repository);
+//
+//    @Test
+//    @DisplayName("Test if find all methods retrieve all students")
+//    public void testFindAllStudents(){
+//        Set<Student> studentsList = this.service.findAll();
+//        Assertions.assertFalse(studentsList.isEmpty());
+//    }
+
+}
